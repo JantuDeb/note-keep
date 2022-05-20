25bef9697492e18d7018f7e7e787d15a10afa3bb
Jantu Deb <jantud54@gmail.com>
2022-05-20 14:21:23 +0530

    added loading, private route, modal screen

---
+++ src/component/loader/Loader.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Loader.css";
+const Loader = () => {
+  return (
+    <div className="flex center w-full">
+      <div className="loader">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
